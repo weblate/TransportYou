8c4476f6f941542fc07efe1b271dbf35fbe25fd0
Bnyro <bnyro@tutanota.com>
2026-05-20 18:43:09 +0200

    chore: bump version to v4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "net.youapps.transport"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "4.0"
+        versionCode = 11
+        versionName = "4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
