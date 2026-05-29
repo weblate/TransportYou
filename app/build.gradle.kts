672129678bf115b5d764b5e87085051ad62c3975
Bnyro <bnyro@tutanota.com>
2026-05-29 14:34:30 +0200

    chore: bump version to 4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "net.youapps.transport"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "4.1"
+        versionCode = 12
+        versionName = "4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
